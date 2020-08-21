680b6a9fc2e5b8e02c74dec7584c5f2ece4dbc12
HCFerraz <henriquecabralferraz@gmail.com>
2020-08-21 20:17:58 -0300

    Cards full

--- src/routes/postRoutes/routes.js
+++ src/routes/postRoutes/routes.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 const User = require('../../schemas/userSchema');
+const { Deck, Card } = require('../../schemas/deckSchema')
 
 const passwordAuthorizated = async (req, res) => {
     const password = req.header('Password');
@@ -24,7 +25,7 @@ router.post('/signUp', async (req, res) => {
         return;
     }
     if (pswd) {
-        res.status(200).json({ message: 'Wrong password'})
+        res.status(200).json({ message: 'Wrong password' })
         return
     }
 
@@ -41,8 +42,48 @@ router.post('/signUp', async (req, res) => {
 });
 
 router.post('/newCard', async (req, res) => {
-    // if(!await passwordAuthorizated(req, res)) return
-    const newCard = req.body
+    let card = []
+    for (let i = 0; i < req.body.cards.length; i++) {
+        card.push(new Card({
+            frontSide: req.body.cards[i].front,
+            backSide: req.body.cards[i].back
+        }))
+    }
+    const newDeck = new Deck({
+        categorie: req.body.categorie,
+        name: req.body.name,
+        description: req.body.description,
+        image: req.body.image,
+        cards: card
+    })
+    try {
+        newDeck.save()
+        res.status(201).json({ message: 'Card created successfully' })
+    } catch (error) {
+        res.status(400).json({ message: error.message })
+    }
 })
+// router.post('/newCard', async (req, res) => {
+//     const newDeck = new Deck({
+//         categorie: req.body.categorie,
+//         name: req.body.name,
+//         description: req.body.description,
+//         image: req.body.image,
+//         cards: [{
+//             frontSide: req.body.cards[0].front,
+//             backSide: req.body.cards[0].back
+//         },
+//         {
+//             frontSide: req.body.cards[1].front,
+//             backSide: req.body.cards[1].back
+//         }]
+//     })
+//     try {
+//         newDeck.save()
+//         res.status(201).json({ message: 'Card created successfully' })
+//     } catch (error) {
+//         res.status(400).json({ message: error.message })
+//     }
+// })
 
 module.exports = router
